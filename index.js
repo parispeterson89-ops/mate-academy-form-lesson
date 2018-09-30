a704e1bf5f606505328cb4096a70d34fda5f8ead
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2018-09-30 19:27:43 +0300

    setup heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // content of index.js
 const http = require('http');
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
